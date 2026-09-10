866ab646a18807ed3400e93084e9b14ebeeb303c
JoaoSousa2002 <joao21senac@gmail.com>
2026-09-10 01:53:03 -0300

    Adição de comentarios no Servidor js

--- src/rf-001-Login/Autenticacao.js
+++ src/rf-001-Login/Autenticacao.js
@@ -1,10 +1,12 @@
 import bcrypt from 'bcrypt'
 
+// Função para gerar uma senha Hash
 async function gerarHashSenha(senhaTextoPuro) {
   const saltRounds = 10;
   return await bcrypt.hash(senhaTextoPuro, saltRounds);
 }
 
+// Função para comparar a senha digitada com a senha hash do banco
 async function validarSenha(senhaDigitada, senhaHashDoBanco) {
   return await bcrypt.compare(senhaDigitada, senhaHashDoBanco);
 }
--- src/rf-001-Login/Servidor.js
+++ src/rf-001-Login/Servidor.js
@@ -16,20 +16,22 @@ import { readFileSync } from 'fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app = Express()
 
+// Cors para autorizar acesso local para a porta 5500 (Padrão do live server)
+const app = Express()
 app.use(cors({ origin: 'http://127.0.0.1:5500' }));
 app.use(Express.json())
-
 const swaggerSpec = JSON.parse(
     readFileSync(new URL('../../docs/api/API-SWAGGER.json', import.meta.url))
 );
+
+//Rota da API SWAGGER UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// rotas de URL para direcionar o front-end
 app.get('/', (req, res) => res.redirect('/login'));
 app.use('/login', Express.static(path.join(__dirname, 'public')))
 app.use('/cadastro-usuario', Express.static(path.join(__dirname, '../rf-002-Cadastro_usuario/public')))
-
 
 
 const supabase = createClient(
@@ -42,9 +44,13 @@ const supabase = createClient(
     }
 )
 
+
+// Rotas da api
 app.post('/login', async (req, res) => {
     const { email, senha } = req.body;
 
+
+    // Verifica se email ou senha estão vazios
     if (!email || !senha) {
         return res.status(400).json({ mensagem: 'Email e senha são obrigatórios' });
     }
@@ -55,41 +61,53 @@ app.post('/login', async (req, res) => {
         .eq('email', email)
         .single();
 
+    // Erro se o email não estiver cadastrado
     if (error) {
         // Log só do erro técnico no servidor (nunca a senha do usuário).
         console.log('Erro ao buscar usuário:', error.message);
         return res.status(401).json({ mensagem: 'Email ou senha inválidos' });
     }
 
+    // Compara a senha digitada com a senha hash do banco, retorna true ou false
     const senhaCorreta = await validarSenha(senha, usuario.senha_hash);
-
     if (!senhaCorreta) {
         return res.status(401).json({ mensagem: 'Email ou senha inválidos' });
+    } else {
+        res.status(200).json({
+            mensagem: 'Login realizado com sucesso',
+            usuario: { id: usuario.id_usuario, nome: usuario.nome }
+        });
+
     }
 
-    res.status(200).json({
-        mensagem: 'Login realizado com sucesso',
-        usuario: { id: usuario.id_usuario, nome: usuario.nome }
-    });
 })
 
 app.post('/cadastro-usuario', async (req, res) => {
     const { nome, email, senha } = req.body;
 
+    // Mesmos testes são feitos no front-end (index do RF-002), porem tambem adicionei no backend para segurança
+    // !nome - Se nome estiver vazio
+    // !email - Se email estiver vazio
+    // !email.includes ('@') - Se o email contem '@'
+    // !senha - Se a senha estiver vazio
+    // senha.lent < 10 - Se a senha tiver menos de 10 caracteres
     if (!nome || !email || !email.includes('@') || !senha || senha.length < 10) {
         return res.status(400).json({ mensagem: 'Dados inválidos ou incompletos' });
     }
 
+    // Verifica se o email recebido já está cadastrado e retorna erro com status 409 se sim
     const { data } = await supabase
         .from('usuarios')
         .select('email')
         .eq('email', email)
-        .maybeSingle()
+        .maybeSingle()  // Se a lista existir, retorna ela, se não, não retorna nada
 
 
     if (data) {
         res.status(409).json({ mensagem: "Esse email já está cadastrado" })
-    } else {
+    }
+    // se o email não está cadastrado, continua com o cadastro
+    else {
         const { error } = await supabase
             .from('usuarios')
             .insert({
